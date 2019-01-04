5518549944f05106f8ddec59dfa9df452e6f9f72
ymgn <gonyamagon@gmail.com>
2019-01-04 15:25:15 +0900

    add inputrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/.git-prompt.sh ~/.git-prompt.sh
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.git-completion.bash ~/.git-completion.bash
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.inputrc ~/.inputrc
